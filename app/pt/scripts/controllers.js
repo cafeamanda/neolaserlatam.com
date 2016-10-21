864d61066831614cbfedc6a7f2db2806eb1a2704
cafeamanda <amandadearaujo0@gmail.com>
2016-10-21 14:19:47 -0200

    neolaser_pt-br

--- app/pt/scripts/controllers.js
+++ app/pt/scripts/controllers.js
@@ -3,7 +3,11 @@
 angular.module('neoApp_pt-br')
 
 .controller('PageCtrl', ['$scope', function ($scope) {
-  
+
   console.log("Page Controller reporting for duty.");
 
+}])
+
+.controller('TreatCtrl', ['$scope', function($scope) {
+  console.log("Treatment Controller reporting for duty.");
 }]);
